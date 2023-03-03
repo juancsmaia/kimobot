79effca843410284f613b9f904564a82e8763611
Juan Maia <juan.maia@diazerosecurity.com.br>
2023-03-02 22:50:45 -0300

    um monte de coisa nova

---
+++ build/resources/main/db/migration/V0__initial.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `roulette` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `active` bit NOT NULL DEFAULT 0,
+  `guild_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `anime` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(5000) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `roulette_index` int NOT NULL,
+  `episodes` int NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `watched` int NOT NULL,
+  `roulette_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY (`roulette_id`),
+  CONSTRAINT FOREIGN KEY (`roulette_id`) REFERENCES `roulette` (`id`)
+);
+
+CREATE TABLE `user_token` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `id_user` varchar(255) UNIQUE NOT NULL,
+    `token` longtext,
+    PRIMARY KEY (`id`)
+);
